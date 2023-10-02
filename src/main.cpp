73223c26bacb8264fcd4ddaaf4b2e1c67a8d2c2c
ZURA-Dev <thaplaya03@gmail.com>
2023-10-02 15:09:51 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+// todos
+// add a option for the user to read files into a hashmap? or read into multiple variables if seperated by like a :
 class fileOperations
 {
 private:
@@ -11,7 +12,7 @@ private:
     int currentPageLine = 0;
 
 public:
-    void openfile(std::string filename = "", bool manualInput)
+    void openfile(std::string filename = "", bool manualInput = false)
     {
         if (manualInput == true)
         {
@@ -29,6 +30,7 @@ public:
             std::cout << "Failed to open file: " << file;
             return;
         }
+        std::cout << "successfully opened file: " << file << std::endl;
         successfulFileOpen = true;
     }
     template <typename T>
@@ -36,18 +38,22 @@ public:
     {
         if (successfulFileOpen == false)
         {
-            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << endl;
+            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << std::endl;
             return;
         }
         outputFile << write << std::endl;
     }
     template <typename T>
-    T readFile(T readInto, int numOfLinesToRead = 1, bool readAll = false, bool resetCurrentPageLine = false)
+    T readFile(T readInto, int numOfLinesToRead = 1, bool readAll = false, bool resetCurrentPageLine = false, int boostCurrentLinesRead = 0)
     {
+        if (boostCurrentLinesRead > 0)
+        {
+            currentPageLine = boostCurrentLinesRead;
+        }
         int linesRead = 0;
         if (successfulFileOpen == false)
         {
-            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << endl;
+            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << std::endl;
             return;
         }
         if (resetCurrentPageLine == true)
@@ -74,10 +80,8 @@ public:
     }
 };
 
-template <typename T>
-void writeFile(T write)
-{
-}
 int main()
 {
+    fileOperations files;
+    files.openfile("test.txt", false);
 }
