10472c46a4e45f477729c621d87b35d027fc84c7
ZURA-Dev <thaplaya03@gmail.com>
2023-10-02 14:56:27 -0700

    read,write,open,close file operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,83 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
+class fileOperations
+{
+private:
+    std::string file;
+    std::ofstream outputFile;
+    bool successfulFileOpen = false;
+    int currentPageLine = 0;
+
+public:
+    void openfile(std::string filename = "", bool manualInput)
+    {
+        if (manualInput == true)
+        {
+            std::cout << "input file name: ";
+            std::cin >> file;
+            std::cin.clear();
+        }
+        else
+        {
+            file = filename;
+        }
+        outputFile.open(file);
+        if (!outputFile.is_open())
+        {
+            std::cout << "Failed to open file: " << file;
+            return;
+        }
+        successfulFileOpen = true;
+    }
+    template <typename T>
+    void writeFile(T write)
+    {
+        if (successfulFileOpen == false)
+        {
+            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << endl;
+            return;
+        }
+        outputFile << write << std::endl;
+    }
+    template <typename T>
+    T readFile(T readInto, int numOfLinesToRead = 1, bool readAll = false, bool resetCurrentPageLine = false)
+    {
+        int linesRead = 0;
+        if (successfulFileOpen == false)
+        {
+            std::cout << "must insert file name, or file wasnt opened properly. Cant perform operation" << endl;
+            return;
+        }
+        if (resetCurrentPageLine == true)
+        {
+            currentPageLine = 0;
+        }
+        if (readAll == false)
+        {
+            while (linesRead < numOfLinesToRead && std::getline(outputFile, readInto))
+            {
+                linesRead++;
+                currentPageLine++;
+            }
+        }
+        else
+        {
+            while (std::getline(outputFile, readInto))
+                ;
+        }
+    }
+    void closeFile()
+    {
+        outputFile.close();
+    }
+};
+
+template <typename T>
+void writeFile(T write)
+{
+}
 int main()
 {
 }
